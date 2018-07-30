cb68345c2a76c4f9b390e9af75a8cd1a66a8d8b5
Amosraj Josphine Grace Pushparani  (NCS) <josphinegp@ncs.com.sg>
2018-07-30 12:44:14 +0800

    first commit

---
+++ src/com/sample/test/FirstSample.java
@@ -0,0 +1,10 @@
+package com.sample.test;
+
+public class FirstSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My first sample project");
+	}
+
+}
