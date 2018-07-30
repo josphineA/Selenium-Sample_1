bedd7b90f964bb0298484543ac7c1c5585102606
josphinegp <jose.jgpr@gmail.com>
2018-07-30 15:32:35 +0800

    2nd changes

--- src/com/sample/test/FirstSample.java
+++ src/com/sample/test/FirstSample.java
@@ -5,6 +5,7 @@ public class FirstSample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("My first sample project");
+System.out.println("My first sample project");
 	}
 
 }
